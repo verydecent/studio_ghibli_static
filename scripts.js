3eb0ef14583553722e6df3369a9b20cbae38af16
verydecent <wnjhwng@gmail.com>
2019-06-19 01:07:19 -0700

    Successfully requesting data from API, error handling setup

--- scripts.js
+++ scripts.js
@@ -5,7 +5,14 @@ const request = new XMLHttpRequest();
 request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
 
 request.onload = function() {
-  
+  // Initiate access to JSON
+  // >:D
+  const data = JSON.parse(this.response);
+
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach(movie => console.log(movie.title));
+  }
+  else console.log('error');
 }
 
 // Send request
